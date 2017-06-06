e04675d24c493d929aeb21dd009c6e16cdd25d97
Megan B <brown.m.j@outlook.com>
2017-06-06 15:35:36 -0500

    found solution, but not according to instructions, still working

--- boybands.js
+++ boybands.js
@@ -1,35 +1,50 @@
 console.log("hello megan");
+
 var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
 // The number of loops to perform (what if the array changes?)
-var loopCount = 5;
+// var loopCount = 5;
 
 // Keep track of which band we're on in the loop
-var currentBand = "bands[i]";
+// var currentBand = "";
 
 // Keep track of which veggie we're on in the loop
-var currentVeggie = "vegetables[i]";
+// var currentVeggie = "";
 
-// Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById("boy-bands");
-
+// // Get a reference to the appropriate DOM element for bands
+// var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = document.getElementById("vegetables");
+// var veggieElement = document.getElementById("vegetables");
 
 // Start looping
-for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+// for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
   
-  // Add the band names into the correct <div>
-  currentBand = bandElement.innerHTML;
-  currentBand = `<ul><li>${currentBand}</li></ul>`;
+//   Add the band names into the correct <div>
+//   currentBand = bandElement.innerHTML;
+//   currentBand += `<div>${currentBand}</div>`;
 
-  // Add the veggie names into the correct <div>
-  currentVeggie = veggieElement.innerHTML;
-  currentVeggie = `<ul><li>${currentVeggie}</li></ul>`;
-}
+//   Add the veggie names into the correct <div>
+//   currentVeggie = veggieElement.innerHTML;
+//   currentVeggie += `<div>${currentVeggie}</div>`;
+// }
 
 // Loop through the two arrays provided (bands and vegetables) and output each 
 // element in the arrays into their corresponding HTML <div> element. Ensure 
 // that each item is in a block element (e.g. li, div, p. etc...)
+
+for (i = 0; i < bands.length; i++) {
+	document.getElementById("boy-bands").innerHTML +=
+		`<div>${bands[i]}</div>`
+}
+
+for (i = 0; i < vegetables.length; i++) {
+	document.getElementById("vegetables").innerHTML +=
+		`<div>${vegetables[i]}</div>`
+}
+
+
+
+
+
